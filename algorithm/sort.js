ad5a90c99dbeb17f1157ade1cc40deb432fd0500
Graham Kang <celica212@gmail.com>
2024-01-08 10:14:12 +0900

    update

---
+++ algorithm/sort.js
@@ -0,0 +1,102 @@
+/*
+Bubble Sort
+
+6 5 4 3 2 1
+1. 5 4 3 2 1 6
+2. 4 3 2 1 5 6
+3. 3 2 1 4 5 6
+4. 2 1 3 4 5 6
+5. 1 2 3 4 5 6
+
+n * (n - 1)
+= O(n^2)
+*/
+function bubbleSort(arr) {
+    const n = arr.length
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                const temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+            }
+        }
+    }
+    return arr
+}
+
+/*
+Selection Sort
+
+6 5 4 3 2 1
+
+1. 1 5 4 3 2 6
+2. 1 2 4 3 5 6
+3. 1 2 3 4 5 6
+4. 1 2 3 4 5 6
+
+2 9 8 3 4 7 1
+1. 1 9 8 3 4 7 2
+2. 1 2 8 3 4 7 9
+3. 1 2 3 8 4 7 9
+4. 1 2 3 4 8 7 9
+5. 1 2 3 4 7 8 9
+
+
+n * (n - 1)
+= O(n^2)
+*/
+
+function selectionSort(arr) {
+    const n = arr.length
+
+    for (let i = 0; i < n - 1; i++) {
+        let min = Infinity
+        let minIndex = -1
+        for (let j = i; j < n; j++) {
+            if (arr[j] < min) {
+                min = arr[j]
+                minIndex = j
+            }
+        }
+        const temp = arr[i]
+        arr[i] = arr[minIndex]
+        arr[minIndex] = temp
+    }
+
+    return arr
+}
+
+/*
+2 9 8 3 4 7 1
+
+2
+2 9
+2 8 9
+2 3 8 9
+2 3 4 8 9
+2 3 4 7 8 9 
+1 2 3 4 5 7 8 9
+
+= O(n^2)
+*/
+
+function insertionSort(arr) {
+    const n = arr.length
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i; j > 0; j--) {
+            if (arr[j] >= arr[j - 1]) break
+            const temp = arr[j]
+            arr[j] = arr[j - 1]
+            arr[j - 1] = temp
+        }
+    }
+
+    return arr
+}
+
+
+console.log(bubbleSort([6, 5, 1, 4, 2, 8, 3]))
+console.log(selectionSort([6, 5, 1, 4, 2, 8, 3]))
+console.log(insertionSort([6, 5, 1, 4, 2, 8, 3]))
